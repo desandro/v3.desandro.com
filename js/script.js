3800c735b373fe36bdaf5c88e6f62f8ad52548c5
David DeSandro <desandrocodes@gmail.com>
2012-07-22 18:57:24 -0400

    added physics to particles

--- js/script.js
+++ js/script.js
@@ -11,6 +11,82 @@
 
 var charElems = [];
 var charParticles = [];
+var mouseX, mouseY;
+var isMouseDown = false;
+
+// -------------------------- CharParticle -------------------------- //
+
+function CharParticle( elem, index ) {
+  this.index = index;
+  this.element = elem;
+  this.width = elem.offsetWidth;
+  this.height = elem.offsetHeight;
+  this.originX = elem.offsetLeft + this.width / 2;
+  this.originY = elem.offsetTop + this.height / 2;
+  this.x = 0;
+  this.y = 0;
+  this.deltaX = 0;
+  this.deltaY = 0;
+
+  // console.log( this.originX, this.originY );
+
+  this.velocityX = 0;
+  this.velocityY = 0;
+
+}
+
+CharParticle.prototype.update = function() {
+  // console.log('updating');
+  // Attracted to mouse
+  var dx = mouseX - this.x;
+  var dy = mouseY - this.y;
+  var d = Math.sqrt( dx*dx + dy*dy );
+  var maxDistance = 200;
+
+  if ( isMouseDown && d < maxDistance ) {
+    // dx = mouseX - p.x;
+    // dy = mouseY - p.y;
+    var force = 1 - d / maxDistance;
+    var angle = Math.atan2( dy, dx );
+    this.velocityX += Math.cos( angle ) * -force;
+    this.velocityY += Math.sin( angle ) * -force;
+  } else {
+    // Attracted to start position
+    this.velocityX += ( 0 - this.deltaX ) * 0.02;
+    this.velocityY += ( 0 - this.deltaY ) * 0.02;
+  }
+
+  // Integrate velocity
+  this.deltaX += this.velocityX;
+  this.deltaY += this.velocityY;
+
+  // Apply friction
+  this.velocityX *= 0.95;
+  this.velocityY *= 0.95;
+
+  // Update position
+  // p.domElement.css({
+  //     left: p.x,
+  //     top: p.y
+  // });
+
+  this.x = this.originX + this.deltaX;
+  this.y = this.originY + this.deltaY;
+
+  // this.element.style.left = this.deltaX + 'px';
+  // this.element.style.top  = this.deltaY + 'px';
+
+  this.element.style.WebkitTransform =
+    'translate3d(' + this.deltaX + 'px, ' + this.deltaY + 'px, 0)';
+
+  if (!this.index) {
+    // console.log( ~~this.x, ~~this.y );
+  }
+
+};
+
+
+// -------------------------- setupCharElems -------------------------- //
 
 function setupCharElems() {
   var splitables = document.querySelectorAll('.split');
@@ -61,14 +137,39 @@ function onBodyClick( event ) {
 }
 
 
-// -------------------------- CharParticle -------------------------- //
+// -------------------------- animation -------------------------- //
 
-function CharParticle( elem ) {
-  this.element = elem;
-  this.width = elem.offsetWidth;
-  this.height = elem.offsetHeight;
-  this.x = elem.offsetLeft + this.width / 2;
-  this.y = elem.offsetTop + this.height / 2;
+function animate() {
+  // console.log('animate');
+  // console.log( charParticles );
+  for ( var i=0, len = charParticles.length; i < len; i++ ) {
+    charParticles[i].update();
+  }
+  setTimeout( animate, 20 );
+}
+
+// -------------------------- events -------------------------- //
+
+function onMousedown( event ) {
+  isMouseDown = true;
+  mouseX = event.pageX;
+  mouseY = event.pageY;
+  // console.log( mouseX, mouseY );
+  event.preventDefault();
+  window.addEventListener( 'mousemove', onMousemove, false );
+  window.addEventListener( 'mouseup', onMouseup, false );
+}
+
+function onMousemove( event ) {
+  mouseX = event.pageX;
+  mouseY = event.pageY;
+  // console.log( mouseX, mouseY );
+}
+
+function onMouseup( event ) {
+  isMouseDown = false;
+  window.removeEventListener( 'mousemove', onMousemove, false );
+  window.removeEventListener( 'mouseup', onMouseup, false );
 }
 
 // -------------------------- init -------------------------- //
@@ -78,11 +179,14 @@ function init() {
   // setup char particles
   var charParticle;
   for ( var i=0, len = charElems.length; i < len; i++ ) {
-    charParticle = new CharParticle( charElems[i] );
+    charParticle = new CharParticle( charElems[i], i );
+    charParticles.push( charParticle );
   }
 
+  document.addEventListener( 'mousedown', onMousedown, false );
 
-  document.body.addEventListener( 'click', onBodyClick, false );
+  animate();
+
 }
 
 
