9476151a646c88c00989a8ed0a85f12286e935b5
David DeSandro <desandrocodes@gmail.com>
2013-07-18 09:07:19 -0400

    first pass at new grunt

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+
+/*jshint node: true*/
+
+module.exports = function( grunt ) {
+
+  var scripts = [
+    'js/base.js',
+    'js/sparkleshine.js',
+    'js/charparticle.js',
+    'js/typekit.js',
+    'js/init.js'
+  ];
+
+  // add bower scripts
+  var concatScripts = [ 'bower_components/requestanimationframe/requestanimationframe.js' ]
+    .concat( scripts );
+
+  grunt.initConfig({
+
+    jshint: {
+      site: [
+        'js/base.js',
+        'js/sparkleshine.js',
+        'js/charparticle.js',
+        'js/typekit.js',
+        'js/init.js'
+      ],
+      options: grunt.file.readJSON('.jshintrc')
+    },
+
+    concat: {
+      scripts: {
+        // src will be set in package-sources task
+        src: concatScripts,
+        dest: 'build/js/scripts.js'
+      }
+    },
+
+    uglify: {
+      scripts: {
+        src: concatScripts,
+        dest: 'build/js/scripts.min.js'
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+
+};
